d48663c6203c055130a1a6c6ea3dd1f336c9f846
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-03-17 18:11:25 +0100

    serialized database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const express = require ('express');
 const app = express();
+const db = require('./database.js')
 
 let port = 3000;
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/src/lab4')
+
+app.get('/admin', (req, res) => {
+    res.render('admin.ejs', {users: users})
+})
 
 app.listen(port, function(){
     console.log(`Server is listening on ${port}...`)
